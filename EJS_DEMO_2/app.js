8ab38905d31a907656a90af80186f67de796f132
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-10-11 22:26:53 +0530

    add partials

--- EJS_DEMO_2/app.js
+++ EJS_DEMO_2/app.js
@@ -2,20 +2,24 @@ var express = require("express");
 var app = express();
 
 app.get("/",function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs"); //  by writing this we don't want to add .ejs on files
 
 app.get("/posts",function(req, res){
     var posts =[
         {title:"half girlfriend",author:"chetan bhagat"},
         {title: "the bad blood",author:"john carreyrou"}
     ];
-    res.render("posts.ejs",{posts:posts});
+    res.render("posts",{posts:posts});
 });
 
 app.get("/fellinlovewith/:thing",function(req, res){
     var thing = req.params.thing;
-    res.render("love.ejs",{thingVar: thing});
+    res.render("love",{thingVar: thing});
 });
 
 app.listen(3000,function(){
