f9d92da51093714e9b6b5adc7eb986f2cff01273
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-10-27 10:46:08 +0530

    full movie search app

--- movie_search_api/app.js
+++ movie_search_api/app.js
@@ -3,9 +3,14 @@ const app = express();
 const request = require("request");
 app.set("view engine","ejs");
 
+app.get("/",function(req,res){
+    res.render("search");
+});
 
 app.get("/results",function(req,res){
-    request("http://www.omdbapi.com/?s=harry&apikey=f10fff11",function(error,response,body){
+    var result=req.query.search;
+    var url = "http://www.omdbapi.com/?s="+result+"&apikey=f10fff11";
+    request(url,function(error,response,body){
         if(!error && response.statusCode == 200){
             var data = JSON.parse(body);
             // res.send(parsedData["Search"][0]["Title"]);
