15ae3ff3089692a3388b6a7f50b347bb406f13a2
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-10-30 16:02:25 +0530

    add mongoose

--- social/v2/app.js
+++ social/v2/app.js
@@ -10,15 +10,35 @@ app.use(express.static("public"));
 
 app.set("view engine","ejs");
 
+// SCHEMA SETUP
+var postsSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-var posts = [
-    {name: "Ashi",image: "https://i.pinimg.com/236x/63/d2/4b/63d24b8198b2a949faf73b7886886d15.jpg"},
-    {name: "Archi",image: "https://i.pinimg.com/236x/dc/9f/25/dc9f251d735fac3d8a8497773a30a1ba.jpg"},
-    {name: "Noni",image: "https://i.pinimg.com/236x/bd/2f/fa/bd2ffaa2743f756e7177795d16da9a8b.jpg"},
-    {name: "Ashi",image: "https://i.pinimg.com/236x/63/d2/4b/63d24b8198b2a949faf73b7886886d15.jpg"},
-    {name: "Archi",image: "https://i.pinimg.com/236x/dc/9f/25/dc9f251d735fac3d8a8497773a30a1ba.jpg"},
-    {name: "Noni",image: "https://i.pinimg.com/236x/bd/2f/fa/bd2ffaa2743f756e7177795d16da9a8b.jpg"}
-]
+var Post = mongoose.model("Post",postsSchema);
+
+// Post.create(
+//     {
+//     name: "Ashi",
+//     image: "https://i.pinimg.com/236x/63/d2/4b/63d24b8198b2a949faf73b7886886d15.jpg"
+// },function(err,post){
+//     if(err){
+//         console.log(err);
+//     } else{
+//         console.log("NEWLY created post");
+//         console.log(post);
+//     }
+// });
+
+// var posts = [
+//     {name: "Ashi",image: "https://i.pinimg.com/236x/63/d2/4b/63d24b8198b2a949faf73b7886886d15.jpg"},
+//     {name: "Archi",image: "https://i.pinimg.com/236x/dc/9f/25/dc9f251d735fac3d8a8497773a30a1ba.jpg"},
+//     {name: "Noni",image: "https://i.pinimg.com/236x/bd/2f/fa/bd2ffaa2743f756e7177795d16da9a8b.jpg"},
+//     {name: "Ashi",image: "https://i.pinimg.com/236x/63/d2/4b/63d24b8198b2a949faf73b7886886d15.jpg"},
+//     {name: "Archi",image: "https://i.pinimg.com/236x/dc/9f/25/dc9f251d735fac3d8a8497773a30a1ba.jpg"},
+//     {name: "Noni",image: "https://i.pinimg.com/236x/bd/2f/fa/bd2ffaa2743f756e7177795d16da9a8b.jpg"}
+// ]
 
 
 app.get("/",function(req,res){
@@ -26,7 +46,14 @@ app.get("/",function(req,res){
 });
 
 app.get("/posts",function(req,res){ //campgrounds
-    res.render("posts",{posts: posts});
+    //get all posts from db
+    Post.find({},function(err,allposts){
+        if(err){
+            console.log(err);
+        } else {
+         res.render("posts",{posts: allposts});
+        }
+    });
 });
 
 app.post("/posts",function(req,res){
@@ -34,9 +61,17 @@ app.post("/posts",function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newPosts = {name:name, image: image};
-    posts.push(newPosts);
+    //create a new post and save to db
+    Post.create(newPosts,function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/posts");
+        }
+    });
+    // posts.push(newPosts);
     //redirect
-    res.redirect("/posts");
+    // res.redirect("/posts");
 });
 
 app.get("/posts/new",function(req,res){
