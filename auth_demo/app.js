62ee928c7eca80543bd7659f2da6bbf35e3140c4
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-10 16:21:26 +0530

    auth(2)

--- auth_demo/app.js
+++ auth_demo/app.js
@@ -1,9 +1,30 @@
-const express = require("express");
-const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/auth_demo', {useNewUrlParser: true});
+const   express                 = require("express"),
+        mongoose                = require("mongoose"),
+        passport                = require("passport"),
+        bodyParser              = require("body-parser"),
+        User                    = require("./models/user"),
+        LocalStrategy           = require("passport-local"),
+        passportLocalMongoose   = require("passport-local-mongoose")
+
+
+mongoose.connect('mongodb://localhost/auth_demo', {useUnifiedTopology: true, useNewUrlParser: true});
 
 const app = express();
 app.set("view engine","ejs");
+
+app.use(require("express-session")({
+    secret:"i am failure",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
 
 app.get("/",function(req,res){
     res.render("home");
