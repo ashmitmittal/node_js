fad359553ac99f8f259b2b768b1ec5b0bfc4d96b
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-24 16:01:48 +0530

    v10 init

---
+++ social/v10/app.js
@@ -0,0 +1,53 @@
+const express           = require("express"),
+      app               = express(),
+      bodyParser        = require("body-parser"),
+      mongoose          = require("mongoose"),
+      passport          = require("passport"),
+      LocalStrategy     = require("passport-local"),
+      Post              = require("./models/post"),
+      Comment           = require("./models/comment"),
+      User              = require("./models/user"),
+      seedDB            = require("./seeds");
+
+//require routes
+var commentRoutes = require("./routes/comments"),
+    postsRoutes   = require("./routes/posts"),
+    authRoutes    = require("./routes/index");
+
+mongoose.connect('mongodb://localhost/social', {useUnifiedTopology: true, useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.set("view engine","ejs");
+
+//all posts will remove form this
+// seedDB(); // seed the database
+
+//PASSPORT CONFIGURATION
+app.use(require("express-session")({
+    secret:"i am losser",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){ //middleware that run for every single route
+    res.locals.currentUser = req.user;
+    next();
+});
+
+
+app.use(authRoutes);
+app.use("/posts",postsRoutes);
+app.use("/posts/:id/comments",commentRoutes);
+
+
+app.listen(3000,function(){
+    console.log("Social app started at port 3000!");
+});
---
+++ social/v10/routes/posts.js
@@ -0,0 +1,67 @@
+var express = require("express");
+var router  = express.Router();
+var Post    = require("../models/post");
+
+// INDEX- show all posts of socialMedia
+router.get("/",function(req,res){ //campgrounds
+    //get all posts from db
+    Post.find({},function(err,allposts){
+        if(err){
+            console.log(err);
+        } else {
+         res.render("posts/index",{posts: allposts});
+        }
+    });
+});
+
+// CREATE - add new posts to DB
+router.post("/", isLoggedIn ,function(req,res){
+    //get data from form and add to posts
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newPosts = {name:name, image: image,description: desc,author: author};
+    //create a new post and save to db
+    Post.create(newPosts,function(err,newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/posts");
+        }
+    });
+    // posts.push(newPosts);
+    //redirect
+    // res.redirect("/posts");
+});
+
+//NEW - show form to create a new post
+router.get("/new", isLoggedIn ,function(req,res){
+    res.render("posts/new");
+});
+
+//SHOW - shows more info about one post
+router.get("/:id",function(req,res){
+    //find the post with provided ID
+    Post.findById(req.params.id).populate("comments").exec(function(err,foundpost){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that post
+            res.render("posts/show",{post:foundpost});
+        }
+    });
+});
+
+function isLoggedIn(req,res,next){   //middleware
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
+
+
+module.exports = router;
