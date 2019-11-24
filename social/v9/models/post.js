2e8c92162312b0d4c7d31ddaba2c43795540017e
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-24 13:18:46 +0530

    v9

---
+++ social/v9/models/post.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+// SCHEMA SETUP
+var postsSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String,
+    author: {
+        id:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    },
+    comments:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+});
+
+//compiling schema into model
+module.exports = mongoose.model("Post",postsSchema);
