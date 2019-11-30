ec448baf1d5119d8381f44f55793566bc3409509
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-11-30 13:18:47 +0530

    todo_api 1

---
+++ todo_api/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+var port = process.env.PORT || 3000;
+var bodyParser = require("body-parser");
+
+var todoRoutes = require("./routes/todos");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api/todos',todoRoutes);
+app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname+'/views'));
+
+
+app.get("/",function(req,res){
+    res.sendFile("index.html");
+});
+
+
+app.listen(port,function(){
+    console.log("Running on PORT "+port);
+})
---
+++ todo_api/public/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos);
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    });
+
+    $('.list').on('click','span',function(){
+        removeTodo($(this).parent());
+    })
+});
+
+function addTodos(todos) {
+    //add todod to page
+    todos.forEach(function(todo){
+        addTodo(todo);
+    });
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="task">'+todo.name+'<span>X</span><li>');
+    newTodo.data('id',todo._id);
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo(){
+    var userInput = $('#todoInput').val();
+    $.post('/api/todos',{name:userInput})
+    .then(function(newTodo){
+        $('#todoInput').val('');
+        addTodo(newTodo);
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = "/api/todos/"+clickedId;
+    // $(this).parent().remove();
+    $.ajax({
+        method: 'DELETE',
+        url: deleteUrl
+    })
+    .then(function(data){
+        todo.remove();
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
